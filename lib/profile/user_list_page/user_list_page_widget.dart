588a755976d9c07387eb3b1b09d8ea4b33ad7797
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-25 17:39:49 +0000

    Updating to latest FlutterFlow output.

--- lib/profile/user_list_page/user_list_page_widget.dart
+++ lib/profile/user_list_page/user_list_page_widget.dart
@@ -4,7 +4,7 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/profile/user_list_view/user_list_view_widget.dart';
+import '/actions/actions.dart' as action_blocks;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
@@ -160,12 +160,235 @@ class _UserListPageWidgetState extends State<UserListPageWidget> {
                     (userRefToProcessIndex) {
                   final userRefToProcessItem =
                       userRefToProcess[userRefToProcessIndex];
-                  return UserListViewWidget(
-                    key: Key(
-                        'Keypys_${userRefToProcessIndex}_of_${userRefToProcess.length}'),
-                    queryType: widget.queryType!,
-                    userToShowDataFor: userRefToProcessItem,
-                    userForWhomTheDataRelates: widget.account!,
+                  return StreamBuilder<UsersRecord>(
+                    stream: UsersRecord.getDocument(userRefToProcessItem),
+                    builder: (context, snapshot) {
+                      // Customize what your widget looks like when it's loading.
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: SizedBox(
+                            width: 50.0,
+                            height: 50.0,
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                FlutterFlowTheme.of(context).primary,
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                      final containerUsersRecord = snapshot.data!;
+                      return Material(
+                        color: Colors.transparent,
+                        elevation: 2.0,
+                        child: Container(
+                          width: double.infinity,
+                          height: 60.0,
+                          decoration: BoxDecoration(
+                            color:
+                                FlutterFlowTheme.of(context).primaryBackground,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                12.0, 8.0, 12.0, 8.0),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                InkWell(
+                                  splashColor: Colors.transparent,
+                                  focusColor: Colors.transparent,
+                                  hoverColor: Colors.transparent,
+                                  highlightColor: Colors.transparent,
+                                  onTap: () async {
+                                    logFirebaseEvent(
+                                        'USER_LIST_CircleImage_74l464b3_ON_TAP');
+                                    if (userRefToProcessItem ==
+                                        currentUserReference) {
+                                      logFirebaseEvent(
+                                          'CircleImage_navigate_to');
+
+                                      context.pushNamed('ProfilePage');
+                                    } else {
+                                      logFirebaseEvent(
+                                          'CircleImage_navigate_to');
+
+                                      context.pushNamed(
+                                        'OtherUserPFP',
+                                        queryParameters: {
+                                          'pageUserRef': serializeParam(
+                                            userRefToProcessItem,
+                                            ParamType.DocumentReference,
+                                          ),
+                                        }.withoutNulls,
+                                      );
+                                    }
+                                  },
+                                  child: Container(
+                                    width: 40.0,
+                                    height: 40.0,
+                                    clipBehavior: Clip.antiAlias,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: Image.network(
+                                      containerUsersRecord.photoUrl,
+                                      fit: BoxFit.fitWidth,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: EdgeInsetsDirectional.fromSTEB(
+                                        12.0, 0.0, 0.0, 0.0),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          containerUsersRecord.displayName,
+                                          style: FlutterFlowTheme.of(context)
+                                              .bodyLarge
+                                              .override(
+                                                fontFamily: 'Plus Jakarta Sans',
+                                                color:
+                                                    FlutterFlowTheme.of(context)
+                                                        .primaryText,
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w500,
+                                                useGoogleFonts: GoogleFonts
+                                                        .asMap()
+                                                    .containsKey(
+                                                        FlutterFlowTheme.of(
+                                                                context)
+                                                            .bodyLargeFamily),
+                                              ),
+                                        ),
+                                        Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding: EdgeInsetsDirectional
+                                                  .fromSTEB(0.0, 4.0, 0.0, 0.0),
+                                              child: Text(
+                                                '@',
+                                                style:
+                                                    FlutterFlowTheme.of(context)
+                                                        .labelMedium
+                                                        .override(
+                                                          fontFamily:
+                                                              'Plus Jakarta Sans',
+                                                          color: FlutterFlowTheme
+                                                                  .of(context)
+                                                              .primary,
+                                                          fontSize: 14.0,
+                                                          fontWeight:
+                                                              FontWeight.w500,
+                                                          useGoogleFonts: GoogleFonts
+                                                                  .asMap()
+                                                              .containsKey(
+                                                                  FlutterFlowTheme.of(
+                                                                          context)
+                                                                      .labelMediumFamily),
+                                                        ),
+                                              ),
+                                            ),
+                                            Expanded(
+                                              child: Padding(
+                                                padding: EdgeInsetsDirectional
+                                                    .fromSTEB(
+                                                        0.0, 4.0, 0.0, 0.0),
+                                                child: Text(
+                                                  containerUsersRecord.userName,
+                                                  style: FlutterFlowTheme.of(
+                                                          context)
+                                                      .labelMedium
+                                                      .override(
+                                                        fontFamily:
+                                                            'Plus Jakarta Sans',
+                                                        color:
+                                                            FlutterFlowTheme.of(
+                                                                    context)
+                                                                .primary,
+                                                        fontSize: 14.0,
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                        useGoogleFonts: GoogleFonts
+                                                                .asMap()
+                                                            .containsKey(
+                                                                FlutterFlowTheme.of(
+                                                                        context)
+                                                                    .labelMediumFamily),
+                                                      ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                if (widget.account == currentUserReference)
+                                  Padding(
+                                    padding: EdgeInsetsDirectional.fromSTEB(
+                                        0.0, 0.0, 10.0, 0.0),
+                                    child: FlutterFlowIconButton(
+                                      borderColor:
+                                          FlutterFlowTheme.of(context).primary,
+                                      borderRadius: 200.0,
+                                      borderWidth: 1.0,
+                                      buttonSize: 40.0,
+                                      fillColor:
+                                          FlutterFlowTheme.of(context).primary,
+                                      icon: Icon(
+                                        Icons.person_remove,
+                                        color: FlutterFlowTheme.of(context)
+                                            .primaryText,
+                                        size: 24.0,
+                                      ),
+                                      onPressed: () async {
+                                        logFirebaseEvent(
+                                            'USER_LIST_person_remove_ICN_ON_TAP');
+                                        if (widget.queryType == 'Following') {
+                                          logFirebaseEvent(
+                                              'IconButton_action_block');
+                                          await action_blocks.unfollowUser(
+                                            context,
+                                            userToUnfollow:
+                                                containerUsersRecord.reference,
+                                          );
+                                          setState(() {});
+                                        } else {
+                                          logFirebaseEvent(
+                                              'IconButton_action_block');
+                                          await action_blocks.removeFollower(
+                                            context,
+                                            userToRemove:
+                                                containerUsersRecord.reference,
+                                          );
+                                          setState(() {});
+                                        }
+
+                                        logFirebaseEvent(
+                                            'IconButton_update_widget_state');
+                                        setState(() {
+                                          _model.removeFromListOfUserRefs(
+                                              userRefToProcessItem);
+                                        });
+                                      },
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   );
                 }),
               );
